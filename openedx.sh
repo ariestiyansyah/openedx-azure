f16c723e2e3030adc6b3e4ee8d3ed9796e55322c
ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2017-03-25 00:37:56 +0700

    refactor to ficus

--- openedx.sh
+++ openedx.sh
@@ -1,5 +1,5 @@
 # 1. Set the OPENEDX_RELEASE variable:
-export OPENEDX_RELEASE=the-tag/you-want-to-install
+export OPENEDX_RELEASE=open-release/ficus
 # 2. Bootstrap the Ansible installation:
 wget https://raw.githubusercontent.com/edx/configuration/$OPENEDX_RELEASE/util/install/ansible-bootstrap.sh -O - | sudo bash
 # 3. (Optional) If this is a new installation, randomize the passwords:
